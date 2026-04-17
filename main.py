9a3fa0f9428587c92a82c842737afd71058cd00f
Maew <maew@MacBook-Pro.local>
2026-04-18 01:32:12 +0700

    add dashboard

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ def health():
 if __name__ == '__main__':
     import uvicorn
     uvicorn.run(app, host='0.0.0.0', port=int(os.getenv('PORT', 8000)))
+
+from fastapi.responses import FileResponse
+
+@app.get('/dashboard')
+def dashboard():
+    return FileResponse('dashboard.html')
